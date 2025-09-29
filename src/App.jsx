45310a943ea1878e821f113d29ff538cd6263ad1
Mr-Explorer142 <tahmidalamtamim142@gmail.com>
2025-09-29 11:29:22 +0600

    Add Basic Setup for React-Redux

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="my-container flex justify-center h-screen items-center rounded-2xl">
+        <h1 className="text-4xl font-bold text-amber-600 p-10 text-center">
+          Welcome to React + Tailwind Setup 🔥
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
